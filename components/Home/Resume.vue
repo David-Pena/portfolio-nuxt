84837db51973df02347eb10cd353ad47f9ea4961
David-Pena <dev.davidpe@gmail.com>
2024-07-12 01:32:35 -0700

    chore: remove arrow-right from resume rows

--- components/Home/Resume.vue
+++ components/Home/Resume.vue
@@ -24,24 +24,6 @@
             <!-- <img :src="item.logo" alt="" /> -->
             <p class="fz-13">{{ item.logo }}</p>
           </div>
-          <div class="arrow">
-            <a href="#">
-              <svg
-                class="arrow-right"
-                xmlns="http://www.w3.org/2000/svg"
-                xmlns:xlink="http://www.w3.org/1999/xlink"
-                x="0px"
-                y="0px"
-                viewBox="0 0 34.2 32.3"
-                xml:space="preserve"
-                style="strokewidth: 2"
-              >
-                <line x1="0" y1="16" x2="33" y2="16"></line>
-                <line x1="17.3" y1="0.7" x2="33.2" y2="16.5"></line>
-                <line x1="17.3" y1="31.6" x2="33.5" y2="15.4"></line>
-              </svg>
-            </a>
-          </div>
         </div>
       </div>
     </div>
